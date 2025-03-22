051deb315f52157de2984fb5f7c5fa80eb3d725d
vaishnavi24pixel <vaishnavishm082@gmail.com>
2025-03-22 15:21:10 +0530

    jenkins CI

---
+++ src/main/java/coll/cmrit/App.java
@@ -0,0 +1,13 @@
+package coll.cmrit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
